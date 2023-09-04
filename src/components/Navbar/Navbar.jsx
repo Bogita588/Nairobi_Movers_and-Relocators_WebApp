8717778594cb993aa4aac1a7b8ec8055e6c2a1f4
Bogita Brian <bogitabrian@gmail.com>
2023-09-04 15:09:51 +0300

    Finished styling navigation bar for large and small screens

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,29 @@
-import React from 'react';
+import React , {useState}from 'react';
 
 import './Navbar.css';
 import images from '../../constants/images'
-{/*import {GiHamburgerMenu} from 'react-icons/gi'
-import {MdOutlineRestaurantMenu} from 'react-icons/md' */}
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {MdOutlineRestaurantMenu} from 'react-icons/md' 
 
 
-const Navbar = () => (
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+return(
+
   <nav className="kws__navbar">
-   <div className="kws__navbar-logo">
-    <img src ={images.KWS} alt="" />
-   </div >
-   <ul className="kws__navbar-links">
-    <li className="k__nav1"><a href="#home">Home</a></li>
-    <li className="k__nav1"><a href="#attractions">Attractions</a></li>
-    <li className="k__nav1"><a href="#about">About</a></li>
-    <li className="k__nav1"><a href="#awards">Awards</a></li>
-    <li className="k__nav1"><a href="#contact">Contact</a></li>
-    
-    
-   </ul>
+    <div className="kws__navbar-logo">
+      <img src ={images.KWS} alt="" />
+    </div >
+    <ul className="kws__navbar-links">
+      <li className="k__nav1"><a href="#home">Home</a></li>
+      <li className="k__nav1"><a href="#attractions">Attractions</a></li>
+      <li className="k__nav1"><a href="#about">About</a></li>
+      <li className="k__nav1"><a href="#awards">Awards</a></li>
+      <li className="k__nav1"><a href="#contact">Contact</a></li>
+      
+      
+    </ul>
 
    <div className="kws__navbar-login">
     <a href="#login" className='k__nav1'>Login/Register</a>
@@ -27,8 +31,29 @@ const Navbar = () => (
     <a href="/" className='k__nav1'>Other parks</a>
 
    </div>
+   <div className="kws__navbar-smallscreen">
+        <GiHamburgerMenu color="white" fontSize={25} onClick={() => setToggleMenu(true)} />
+
+        {toggleMenu && (
+        <div className="kws__navbar-smallscreen_overlay flex__center slide-bottom">
+              <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+              <ul className="kws__navbar-smallscreen-links">
+                <li className="k__nav1"><a href="#home">Home</a></li>
+                <li className="k__nav1"><a href="#attractions">Attractions</a></li>
+                <li className="k__nav1"><a href="#about">About</a></li>
+                <li className="k__nav1"><a href="#awards">Awards</a></li>
+                <li className="k__nav1"><a href="#contact">Contact</a></li>
+                
+              
+              </ul>
+            
+        </div>
+      )}
+    
+   </div>
 
   </nav>
-);
+)
+}
 
 export default Navbar;
