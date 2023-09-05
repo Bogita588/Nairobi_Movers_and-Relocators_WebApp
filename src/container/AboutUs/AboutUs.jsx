1b320ab469d235f825b25a4dd97b1459cbd0b638
Bogita Brian <bogitabrian@gmail.com>
2023-09-05 22:02:00 +0300

    Added new code to style the about us component

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -5,26 +5,26 @@ import { images } from '../../constants';
 
 const AboutUs = () => (
   <div className="kws__aboutus kws__bg flex__center section__padding" id="about">
-        <div className="kws__aboutus-overlay flex__center">
-          {/*<img src={images.G} alt="G" /> */}
+        {/*<div className="kws__aboutus-overlay flex__center">
+          <img src={images.G} alt="G" /> 
 
-        </div>
+        </div>*/}
 
      
 
         <div className="kws__aboutus-content flex__center">
-            <div className="kws__aboutus-content_about">
+            <div className="kws__aboutus-content_about section__equal-height">
               <h1>About Us</h1>
-              {/*<img src={images.spoon} alt="about__spoon" className="spoon__img" />*/} 
+              {/*<img src={images.spoon} alt="about__spoon" className="spoon__img" />*/}
               <p>Welcome to our digital workshop, where we sculpt the future, one line of code at a time. We are passionate creators, dedicated to designing and developing web applications that redefine possibilities. With innovation at our core, we thrive on turning ideas into immersive online experiences. Join us on this exciting journey as we code the future into reality.</p>
               <button type="button" className='custom__button'>Learn More</button>
             </div>
-            <div className="kws__aboutus-content_knife flex__center">
-              {/*<img src={images.knife} alt="about__knife" /> */}
+            {/*<div className="kws__aboutus-content_knife flex__center">
+              <img src={images.knife} alt="about__knife" /> 
 
-            </div>
-            <div className="kws__aboutus-content_history">
-              <h1>Our History</h1>
+            </div>*/}
+            <div className="kws__aboutus-content_history section__equal-height">
+              <h1>History</h1>
               {/*<img src={images.spoon} alt="about__spoon" className="spoon__img" /> */}
               <p>Welcome to our digital workshop, where we sculpt the future, one line of code at a time. We are passionate creators, dedicated to designing and developing web applications that redefine possibilities. With innovation at our core, we thrive on turning ideas into immersive online experiences. Join us on this exciting journey as we code the future into reality.</p>
               <button type="button" className='custom__button'>Learn More</button>
